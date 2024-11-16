1449f277ba8d524b064960ed3b1397509d0c5050
Manav2031 <manavmehta2031@gmail.com>
2024-11-16 19:50:05 +0530

    Updated Code

--- src/main/java/com/example/fullstack_backend/controller/UserController.java
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -15,22 +15,22 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/api/user")
+    @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
 
-    @GetMapping("/api/users")
+    @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @GetMapping("/api/user/{id}")
+    @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @PutMapping("/api/user/{id}")
+    @PutMapping("/user/{id}")
     User updateUser(@RequestBody User newUser, @PathVariable Long id) {
         return userRepository.findById(id).map(user -> {
             user.setUsername(newUser.getUsername());
@@ -40,7 +40,7 @@ public class UserController {
         }).orElseThrow(() -> new UserNotFoundException(id));
     }
 
-    @DeleteMapping("/api/user/{id}")
+    @DeleteMapping("/user/{id}")
     String deleteUser(@PathVariable Long id) {
         if(!userRepository.existsById(id)) {
             throw new UserNotFoundException(id);

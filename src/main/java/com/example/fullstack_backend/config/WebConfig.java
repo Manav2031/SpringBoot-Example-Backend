336ec66b987a4ce828d515fb2665b4f335582231
Manav2031 <manavmehta2031@gmail.com>
2024-11-17 09:30:25 +0530

    Updated Code

--- src/main/java/com/example/fullstack_backend/config/WebConfig.java
+++ src/main/java/com/example/fullstack_backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://spring-boot-example-flame.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow preflight OPTIONS method
                         .allowedHeaders("*")
                         .allowCredentials(true)
